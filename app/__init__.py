f00cf356a8cb81ed59f96674020a01233631df5c
Clemente Iraci <clemente.iraci@gmail.com>
2018-03-30 14:59:46 +0200

    added db models and migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 app.config.from_object(Config) # load configuration variables from Config class
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+# models defines struture of the database
+from app import routes, models
